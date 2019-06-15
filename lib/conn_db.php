fa327c4efd73a2a401e24545d401a9283f8706e2
Caio Siqueira <caiosiqueira@outlook.com>
2019-06-15 14:31:54 -0300

    alter db server

--- lib/conn_db.php
+++ lib/conn_db.php
@@ -1,12 +1,12 @@
 <?php
 
-$conMy = mysqli_connect('localhost', 'root', '');
+$conMy = mysqli_connect('u0zbt18wwjva9e0v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'yks3dxejjlf35eia', 'ctak6qjhsadi495o');
 if (!$conMy) {
     die('Não foi possível conectar: ' . mysqli_error());
 }
 
 mysqli_set_charset($conMy, 'utf8');
-mysqli_select_db($conMy, 'pema_data');
+mysqli_select_db($conMy, 'zyzjmym9foub9xvj');
 
 $qmode = "SET SESSION sql_mode = 'PIPES_AS_CONCAT'";
 mysqli_query($conMy, $qmode);
